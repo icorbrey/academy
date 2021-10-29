d0262f5406f97d41eaf1fc26b3b1227abce97fa1
Isaac <icorbrey@gmail.com>
2021-10-29 02:44:15 -0400

    feat: Create proper landing page

---
+++ src/components/ContentColumn.svelte
@@ -0,0 +1,13 @@
+<div><slot /></div>
+
+<style lang="scss">
+	div {
+		width: 1000px;
+
+		@media screen and (max-width: 1048px) {
+			width: 100%;
+			padding-left: 24px;
+			padding-right: 24px;
+		}
+	}
+</style>
