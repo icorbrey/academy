87d4ea61396bd44f74f82bcf4f18a20ee4cb5d17
Isaac <icorbrey@gmail.com>
2021-11-01 19:31:19 -0400

    chore: Install Storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,13 @@ module.exports = {
 		'../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
 	],
 	'addons': [
+		'@storybook/addon-a11y',
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-svelte-csf'
+		'@storybook/addon-svelte-csf',
+		'storybook-addon-outline',
+		'storybook-dark-mode',
+		'storybook-addon-pseudo-states',
 	],
 	'svelteOptions': {
 		'preprocess': import('../svelte.config.js').preprocess
